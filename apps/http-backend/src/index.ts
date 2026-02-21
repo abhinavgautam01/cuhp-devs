ae10052dcd9f34b99f5f60a2508b690bfe17e9f0
abhinavgautam01 <abgautam1017@gmail.com>
2026-02-21 22:56:56 +0530

    added-cors

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,28 +1,39 @@
-import express, { Request, Response, Application } from 'express';
-import 'dotenv/config';
+import express, { Request, Response, Application } from "express";
+import "dotenv/config";
 import { connectDB } from "@repo/db/index.js";
-import cookieParser from 'cookie-parser';
-import router from './routes/auth.routes';
+import cookieParser from "cookie-parser";
+import router from "./routes/auth.routes";
+import cors from "cors";
 
 const app: Application = express();
 
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  }),
+);
+
 app.use(express.json());
 app.use(cookieParser());
-connectDB();
-
-app.use("/auth", router);
-
-app.get("/", (req: Request, res: Response) => {
-    // res.send is now type-checked!
-    res.send("Hello World!");
-});
 
 const start = async () => {
-  await connectDB();
+  try {
+    await connectDB();
+    app.use("/auth", router);
 
-  app.listen(3001, () => {
-    console.log("http://localhost:3001");
-  });
+    app.get("/", (req: Request, res: Response) => {
+      // res.send is now type-checked!
+      res.send("Hello World!");
+    });
+
+    app.listen(3001, () => {
+      console.log("http://localhost:3001");
+    });
+  } catch (error) {
+    console.error("Failed to start server", error);
+    process.exit(1);
+  }
 };
 
 start();
