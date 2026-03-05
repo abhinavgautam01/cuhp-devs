8714f23aedcfc3eb47512c6e65cfe2d3ff32ed9e
Jatin-Asher <jatinsharma2002oct@gmail.com>
2026-03-05 15:05:25 +0530

    init

--- packages/db/src/models/index.ts
+++ packages/db/src/models/index.ts
@@ -4,15 +4,12 @@ import "./problem.model";
 import "./submission.model";
 import "./language.model";
 import "./defaultCode.model";
-<<<<<<< HEAD
 import "./chatroom.model";
 import "./message.model";
 import "./testcase.model";
-=======
 import "./testcase.model";
 import "./chatroom.model";
 import "./message.model";
->>>>>>> 75126bfd65bc74f8e99f9946418d6c68d2285fd8
 
 // export..!
 export { User } from "./user.model";
@@ -20,12 +17,6 @@ export { Problem } from "./problem.model";
 export { Submission } from "./submission.model";
 export { Language } from "./language.model";
 export { DefaultCode } from "./defaultCode.model";
-<<<<<<< HEAD
 export { ChatRoom } from "./chatroom.model";
 export { Message } from "./message.model";
 export { TestCase } from "./testcase.model";
-=======
-export { TestCase } from "./testcase.model";
-export { ChatRoom } from "./chatroom.model";
-export { Message } from "./message.model";
->>>>>>> 75126bfd65bc74f8e99f9946418d6c68d2285fd8
